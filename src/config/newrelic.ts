84b6aadabbbd9caa426f1d4a793df008729c07a6
Laqwan Brodie <lbrodie@gmail.com>
2019-10-25 15:46:50 -0400

    Adding newrelic

---
+++ src/config/newrelic.ts
@@ -0,0 +1,1 @@
+import * as newrelic from 'newrelic'
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+require('newrelic')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
+  Logger.log(`Server listening on port ${process.env.PORT}`);
 }
 bootstrap();
