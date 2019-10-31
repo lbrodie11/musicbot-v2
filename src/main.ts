ab785a833d10c15c97646711da4cddaf7f767175
Laqwan Brodie <lbrodie@gmail.com>
2019-10-31 13:11:58 -0400

    Moved the puppeteer login

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
-const getSpotifyToken = require('./services/facebooklogin')
 require('newrelic')
+
+import puppeteer from 'puppeteer'
+
+import '../config/env';
+
+const { BASE_URL, FACEBOOK_USERNAME, FACEBOOK_PASSWORD } = process.env;
+async function getSpotifyToken() {
+  const browser = await puppeteer.launch({headless: true, defaultViewport: null})
+  const page = await browser.newPage()
+  
+  await page.goto(BASE_URL, {waitUntil: 'networkidle2'})
+  await page.waitFor(5000);
+  await page.click('div a.btn')
+  await page.waitFor('input[name=email]');
+  await page.waitFor(5000);
+  await page.type('input[name=email]', FACEBOOK_USERNAME)
+  await page.waitFor(5000);
+  await page.type('input[name=pass]', FACEBOOK_PASSWORD)
+  await page.waitFor(3000);
+  await page.click('button[name=login]')
+
+  await browser.close()
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
