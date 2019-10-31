7f9d31f0c28fa1eb545198b0e2a06aab97d49c05
Laqwan Brodie <lbrodie@gmail.com>
2019-10-31 13:04:11 -0400

    ading package.json update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+const getSpotifyToken = require('./services/facebooklogin')
 require('newrelic')
 
 async function bootstrap() {
@@ -8,5 +9,7 @@ async function bootstrap() {
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
   Logger.log(`Server listening on port ${process.env.PORT}`);
+  getSpotifyToken()
+  Logger.log('Logged in to Facebook');
 }
 bootstrap();
