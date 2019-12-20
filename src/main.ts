61cf65730a33789e04adbb943f2a4b93498f0420
Laqwan Brodie <lbrodie@gmail.com>
2019-12-19 20:51:19 -0500

    Storing cookies for puppeteer

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import puppeteer from 'puppeteer'
+const fs = require('fs');
+const cookies = require('../cookies.json')
 require('newrelic')
 
 const { BASE_URL, FACEBOOK_USERNAME, FACEBOOK_PASSWORD } = process.env;
 
 async function getSpotifyToken() {
+  
   const browser = await puppeteer.launch({headless: true, defaultViewport: null, args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage()
-  
+  if(Object.keys(cookies).length){
+    await page.setCookie(...cookies)
+
+    await page.goto(BASE_URL, {waitUntil: 'networkidle2'})
+  } else {
   await page.goto(BASE_URL, {waitUntil: 'networkidle2'})
   await page.waitFor(6000);
   await page.click('div a.btn')
@@ -20,6 +27,11 @@ async function getSpotifyToken() {
   await page.type('input[name=pass]', FACEBOOK_PASSWORD)
   await page.waitFor(8000);
   await page.click('button[name=login]')
+  let currentCookies = await page.cookies()
+
+  fs.writeFileSync('../cookies.json', JSON.stringify(currentCookies))
+  
+  }
   Logger.log('Logged in to Facebook from Puppeteer');
   await browser.close()
 }
