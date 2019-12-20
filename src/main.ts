4b867a0395f73faa5e4ed8c5248ca048f0a2348a
Laqwan Brodie <lbrodie@gmail.com>
2019-12-19 23:06:10 -0500

    More minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
-import puppeteer from 'puppeteer'
-import fs from 'fs';
+import pluginStealth from 'puppeteer-extra-plugin-stealth';
+import puppeteer from 'puppeteer-extra'
+// import fs from 'fs';
 require('newrelic')
 
 const { BASE_URL, FACEBOOK_USERNAME, FACEBOOK_PASSWORD } = process.env;
 
 async function getSpotifyToken() {
+  puppeteer.use(pluginStealth());
 
-  const args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-infobars',
-    '--window-position=0,0',
-    '--ignore-certifcate-errors',
-    '--ignore-certifcate-errors-spki-list',
-    '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
-];
+//   const args = [
+//     '--no-sandbox',
+//     '--disable-setuid-sandbox',
+//     '--disable-infobars',
+//     '--window-position=0,0',
+//     '--ignore-certifcate-errors',
+//     '--ignore-certifcate-errors-spki-list',
+//     '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+// ];
 
-const options = {
-    args,
-    headless: true,
-    ignoreHTTPSErrors: true,
-    userDataDir: './tmp'
-};
+// const options = {
+//     args,
+//     headless: true,
+//     ignoreHTTPSErrors: true,
+//     userDataDir: './tmp',
+// };
   
-  const browser = await puppeteer.launch(options);
-  const preloadFile = fs.readFileSync('./config/preload.js', 'utf8');
+  const browser = await puppeteer.launch({headless: true, defaultViewport: null, ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-infobars', '--window-position=0,0', '--ignore-certifcate-errors', '--ignore-certifcate-errors-spki-list',]});
   const page = await browser.newPage()
-  await page.evaluateOnNewDocument(preloadFile);
   await page.goto(BASE_URL, {waitUntil: 'networkidle2'})
   await page.waitFor(6000);
   await page.click('div a.btn')
